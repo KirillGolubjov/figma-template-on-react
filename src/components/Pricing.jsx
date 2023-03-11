3456b0b6ea097c614075de507bee10a8a173a4c6
Kirill Golubjov <kirillcnz@gmail.com>
2023-03-11 11:27:26 +0200

    Hardcoded Pricing and Price by using data

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,83 @@
+const Pricing = () => {
+  return (
+    <section className='container' style={{ marginBottom: '100px' }}>
+      <div className='pricing-header'>
+        <h1>Choose your plan</h1>
+        <p>
+          There are many reasons to get down and start to get depressed about
+          your situation.
+        </p>
+      </div>
+      <div className='pricing-list'>
+        <div className='pricing-box'>
+          <h2>Free</h2>
+          <h1>
+            <span className='pricing-dollar'>$</span>0
+          </h1>
+          <div className='pricing-text'>
+            <p>
+              Free forever
+              <br />
+              Up to 2 users
+              <br />
+              Github
+            </p>
+          </div>
+          <button className='pricing-button'>GET STARTED</button>
+        </div>
+
+        <div className='pricing-box'>
+          <h2>Free</h2>
+          <h1>
+            <span className='pricing-dollar'>$</span>0
+          </h1>
+          <div className='pricing-text'>
+            <p>
+              Free forever
+              <br />
+              Up to 2 users
+              <br />
+              Github
+            </p>
+          </div>
+          <button className='pricing-button'>GET STARTED</button>
+        </div>
+        <div className='pricing-box'>
+          <h2>Free</h2>
+          <h1>
+            <span className='pricing-dollar'>$</span>0
+          </h1>
+          <div className='pricing-text'>
+            <p>
+              Free forever
+              <br />
+              Up to 2 users
+              <br />
+              Github
+            </p>
+          </div>
+          <button className='pricing-button'>GET STARTED</button>
+        </div>
+        <div className='pricing-box'>
+          <h2>Free</h2>
+          <h1>
+            <span className='pricing-dollar'>$</span>0
+          </h1>
+          <div className='pricing-text'>
+            <p>
+              Free forever
+              <br />
+              Up to 2 users
+              <br />
+              Github
+            </p>
+          </div>
+          <button type='button' className='pricing-button'>
+            GET STARTED
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Pricing;
