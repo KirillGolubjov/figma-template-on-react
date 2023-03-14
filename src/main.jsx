7039a4e994b73f64fd882344acf54451bfcf752d
Kirill Golubjov <kirillcnz@gmail.com>
2023-03-14 18:26:56 +0200

    Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
 import { AppProvider } from './context';
+import Router from './Router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <Router />
     </AppProvider>
   </React.StrictMode>
 );
